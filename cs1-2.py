e4604a0e3b6895b9c94e6c387dd11baf1f350d5d
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-13 23:43:41 -0400

    Add file

--- cs1-2.py
+++ cs1-2.py
@@ -1,11 +1,11 @@
 # Ryan Lugo: RJL 10/12/21
 
-principle_investment = int(input("What's the principle investment?: "))
+principle_investment = float(input("What's the principle investment?: "))
 rate_prompt = float(input("What's the interest rate?: "))
-number_of_times = int(input("What's the number of times that interest is compounded per unit?: "))
+number_of_times = float(input("What's the number of times that interest is compounded per unit?: "))
 time = float(input("What's the amount of time the money is invested in?: "))
 
 rate = rate_prompt / 100
-compound_formula = principle_investment ((1 + (rate / number_of_times))**(number_of_times * time)) 
+compound_formula = principle_investment * ((1 + (rate / number_of_times)) **(number_of_times * time))
 
-print("This is the value of the account at this date: ")
+print("This is the value of the account at this date: ",compound_formula)
